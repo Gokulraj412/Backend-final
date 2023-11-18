4aeccf2414c4ff6eb3111de0263b6cf6ed46b539
Gokulraj412 <127767427+Gokulraj412@users.noreply.github.com>
2023-11-19 03:24:13 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -11,9 +11,12 @@ dotenv.config({path:path.join(__dirname,"config/config.env")})
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}))
+app.use((req,res,next)=>{
+    res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
+    next()
+})
 
 app.use('/uploads', express.static(path.join(__dirname,'uploads') ) )
-
 const user = require('./routes/User')
 const astrologer = require('./routes/astrologer')
 const admin = require('./routes/Admin')
@@ -21,6 +24,7 @@ const admin = require('./routes/Admin')
 app.use('/api/v1',user)
 app.use('/api/v1',astrologer)
 app.use('/api/v1',admin);
+
 
 app.use(errorMiddleware)
 module.exports = app
--- routes/astrologer.js
+++ routes/astrologer.js
@@ -1,20 +1,48 @@
 const express = require("express");
-const app = express()
 const {
   registerAstrologer,
   getAllAstrologers,
   deleteAstrologer,
   updateAstrologer,
   activeAstrologer,
-
   getAstrologer,
+  updateAstro
 } = require("../controllers/astrologerController");
-
 const multer = require("multer");
 const router = express.Router();
 const path = require("path");
+// const upload = multer({
+//     storage: multer.diskStorage({
+//       destination: function (req, file, cb) {
+//         // You can choose the destination based on the file type or other conditions
+//         if (file.fieldname === 'files') {
+//           cb(null, path.join(__dirname, '..', 'uploads/astrologer'));
+//         } else if (file.fieldname === 'profilePic') {
+//           cb(null, path.join(__dirname, '..', 'uploads/images'));
+//         } else {
+//           cb(new Error('Invalid fieldname for destination'));
+//         }
+//       },
+//       filename: function (req, file, cb) {
+//         cb(null, file.originalname);
+//       },
+//     }),
+//   });
+// const upload = multer({
+//   storage: multer.diskStorage({
+//     destination: function (req, file, cb) {
+//       // You can choose the destination based on the file type or other conditions
+//       cb(null, path.join(__dirname, "..", "uploads/certificates"));
+//     },
+//     filename: function (req, file, cb) {
+//       cb(null, Date.now() + '-' + file.originalname);
+//     },
+//   }),
+// });
 
-// app.use('/uploads', express.static('uploads'));
+// router
+//   .route("/astrologer/register")
+//   .post(upload.array("certificates"), registerAstrologer);
 
 
 const upload = multer({
@@ -24,13 +52,13 @@ const upload = multer({
       if (file.fieldname === "certificates") {
         cb(null, path.join(__dirname, "..", "uploads/certificates"));
       } else if (file.fieldname === "profilePic") {
-        cb(null, path.join(__dirname, "..", "uploads/profilepic"));
+        cb(null, path.join(__dirname, "../", "uploads/profilepic"));
       } else {
         cb(new Error("Invalid fieldname for destination"));
       }
     },
     filename: function (req, file, cb) {
-      cb(null, Date.now() + file.originalname);
+      cb(null, file.originalname);
     },
   }),
 });
@@ -40,12 +68,16 @@ router.route("/astrologer/register").post(
 
   registerAstrologer
 );
-router.route("/astrologer/allAstrologers").get(getAllAstrologers);
 
+router.route("/astrologer/update/:id").patch(
+  upload.fields([{ name: "certificates" }, { name: "profilePic" }]),
+
+  updateAstrologer
+);
+router.route("/astrologer/allAstrologers").get(getAllAstrologers);
 router.route("/astrologer/getAstrologer/:id").get(getAstrologer);
 router.route("/astrologer/delete/:id").delete(deleteAstrologer);
-router.route("/astrologer/update/:id").put(
-  upload.fields([{ name: "certificates" }, { name: "profilePic" }]),
-  updateAstrologer);
+// router.route("/astrologer/update/:id").patch(updateAstrologer);
+// router.route("/astro/update/:id").patch(updateAstro)
 router.route("/astrologer/state/:id").put(activeAstrologer);
 module.exports = router;
