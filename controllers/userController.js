544e1b0066aa351db2666da96143d6bc6181f0a9
Gokulraj412 <127767427+Gokulraj412@users.noreply.github.com>
2023-11-27 21:57:01 +0530

    Add files via upload

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,21 +3,31 @@ const UserDetail = require('../models/UserDetail')
 const APIFeatures = require("../utils/apiFeatures")
 
 // register user -  {{base_url}}/api/v1/user/register
-exports.registerUser = catchAsyncError(async(req,res,next)=>{
- 
-   
-    let BASE_URL = process.env.BACKEND_URL
-    if(process.env.NODE_ENV === 'production'){
-        BASE_URL = `${req.protocol}://${req.get('host')}`
-    }
-    const user = await UserDetail.create(req.body);
-    console.log(req.body);
-    res.status(200).json({
-        sucess:true,
-        user
-    })
-    
-})
+exports.registerUser = catchAsyncError(async (req, res, next) => {
+  let BASE_URL = process.env.BACKEND_URL;
+
+  if (process.env.NODE_ENV === 'production') {
+    BASE_URL = `${req.protocol}://${req.get('host')}`;
+  }
+
+  // Check if the user already exists
+  const existingUser = await UserDetail.findOne({ phoneNo: req.body.phoneNo });
+
+  if (existingUser) {
+    // User is already registered
+    return next(new ErrorHandler('User already registered', 409)); // 409 Conflict status code
+  }
+
+  // If user doesn't exist, create a new user
+  const user = await UserDetail.create(req.body);
+  console.log(req.body);
+
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
+
 
 // exports.getUserPhone = async (req, res, next) => {
   
@@ -41,25 +51,35 @@ exports.registerUser = catchAsyncError(async(req,res,next)=>{
 //     });
 //   }
 // };
-exports.loginUser = catchAsyncError(async(req, res, next)=>{
-  const{phoneNo}= req.body
-console.log(phoneNo);
-  if(!phoneNo){
-    return next(new ErrorHandler('Please enter valid Mobile No',400))
-  }
-  //finding the user database
+exports.loginUser = catchAsyncError(async (req, res, next) => {
+  const { phoneNo } = req.body;
 
-  const user = await UserDetail.findOne({phoneNo});
-  if(!user){
-    return next(new ErrorHandler('No user found in this Mobile NO ',401))
-  }
- 
-  res.status(200).json({
-   success:true,
-   user
-  }) 
+  console.log(phoneNo);
 
-})
+  try {
+    if (!phoneNo) {
+      throw new ErrorHandler('Please enter a valid Mobile No', 400);
+    }
+
+    // Finding the user in the database
+    const user = await UserDetail.findOne({ phoneNo });
+
+    if (!user) {
+      throw new ErrorHandler('User not registered', 404);
+    }
+
+    // Additional checks, e.g., password verification, can be added here
+
+    res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    next(error); // Pass the error to the error-handling middleware
+  }
+});
+
+
 // getAlluser -  {{base_url}}/api/v1/user/users
 
 exports.getAllUser = catchAsyncError(async(req,res,next)=>{
