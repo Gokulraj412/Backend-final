14ca2e1286de9316aae3d651e153a86f75d48709
Gokulraj412 <127767427+Gokulraj412@users.noreply.github.com>
2023-12-02 13:40:46 +0530

    Add files via upload

--- models/adminModel.js
+++ models/adminModel.js
@@ -1,7 +1,7 @@
 const mongoose = require ('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcrypt');
-
+const jwt = require('jsonwebtoken');
 const adminSchema = new mongoose.Schema({
     email:{
        type:String
@@ -17,6 +17,19 @@ const adminSchema = new mongoose.Schema({
 adminSchema.pre('save',async function(next){
     this.password = await bcrypt.hash(this.password, 10) 
 })
+adminSchema.methods.getJwtToken = function () {
+    const secret = process.env.JWT_SECRET;
+    const payload = {
+        adminId: this._id,
+        email: this.email,
+        // Other payload data if needed
+    };
+    const options = {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+    };
+
+    return jwt.sign(payload, secret, options);
+};
 adminSchema.methods.isValidPassword = async function(enteredPassword){
     return bcrypt.compare(enteredPassword, this.password)
   }
